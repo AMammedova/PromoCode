49e6a448a08109cb5a8344c36b8e04ef7173af71
AMammedova <ayselmemmedova1718@gmail.com>
2023-01-22 15:18:59 +0400

    tabs

--- src/pages/Admin/Generate.jsx
+++ src/pages/Admin/Generate.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 const Generate = () => {
-  return <div>Generate</div>;
+  return <div>
+
+  </div>;
 };
 
 export default Generate;
