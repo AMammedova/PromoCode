568dc104fde03b766420909c39e0ec3ee2555d77
AMammedova <ayselmemmedova1718@gmail.com>
2023-01-22 20:48:44 +0400

    reportchanges

--- src/pages/Admin/List.jsx
+++ src/pages/Admin/List.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React,{useState} from "react";
 import Table from "../../components/Table";
 
 const List = () => {
+  const [source,setSource]=useState('')
   const headers = [
     "Promocode",
     "merchant",
@@ -11,10 +12,32 @@ const List = () => {
     "source",
     "status",
   ];
+  const headers2 = [
+    "Promocode",
+    "merchant",
+    "description",
+    "Count",
+    "Type",
+    "source",
+    "Export",
+  ];
   return (
     <div>
+      <div className=" w-1/4 flex justify-between items-center gap-2 py-20">
+  <label className="whitespace-nowrap">Sort by Source</label>
+  <select value={source} onChange={(e)=>setSource(e.target.value)}>
+
+<option value="US">Telegram Bot</option>
+
+</select>
+</div>
       <Table headers={headers} variant={1} />
+      <Table headers={headers2} variant={1} />
+   
+      
+      
     </div>
+    
   );
 };
 
--- src/pages/Admin/Report.jsx
+++ src/pages/Admin/Report.jsx
@@ -1,7 +1,55 @@
-import React from "react";
-
+import React ,{useState} from "react";
+import styles from '../../../styles/report.module.css'
 const Reports = () => {
-  return <div>Reports</div>;
+  const [merchant,setMerchant]=useState('');
+  const [source,setSource]=useState('');
+  const [dateStart,setDateStart]=useState('');
+  const [status,setStatus]=useState('');
+  return <div className='flex justify-between'>
+   <div  className="w-1/2">
+     <div className="grid grid-cols-2 gap-4 py-5">
+<div className="flex justify-between items-center gap-2">
+  <label className="whitespace-nowrap">Sort by Source</label>
+  <select value={source} onChange={(e)=>setSource(e.target.value)}>
+
+<option value="US">Telegram Bot</option>
+
+</select>
+</div>
+<div className="flex justify-between items-center gap-2">
+  <label className="whitespace-nowrap">Sort by Merchant</label>
+  <select value={merchant} onChange={(e)=>setMerchant(e.target.value)}>
+
+<option value="US">FRYDAY</option>
+
+</select>
+</div>
+
+
+    </div>
+    <div className="grid grid-cols-2 gap-4 py-5">
+<div className="flex justify-between items-center gap-2">
+  <label className="whitespace-nowrap">Sort by Status</label>
+  <select value={status} onChange={(e)=>setStatus(e.target.value)}>
+
+<option value="US">Used</option>
+
+</select>
+</div>
+<div className="flex justify-between items-center gap-2">
+  <label className="whitespace-nowrap">Sort by Date</label>
+  <select value={dateStart} onChange={(e)=>setDateStart(e.target.value)}>
+
+<option value="US">Date</option>
+
+</select>
+</div>
+    </div>
+   </div>
+    <div className=''>
+ <button className="border border-gray-400 px-8 py-3 rounded text-gray-600">Export</button>
+</div>
+  </div>;
 };
 
 export default Reports;
