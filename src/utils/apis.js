47a7cb135506a50b1395160d5507b08f8464d953
Fuad <fuadbayramv@gmail.com>
2023-01-23 02:22:38 +0400

    api config

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -4,13 +4,37 @@ const instance = axios.create({
   baseURL: "/api/Promocode",
 });
 
+const instanceLogin = axios.create({
+  baseURL: "/api/User",
+});
+
 export const Apis = {
   getAll: async () => {
-    const { data } = instance.get("/All");
+    const { data } = await instance.get("/All");
     return data;
   },
   addRandom: async () => {
-    const { data } = instance.post("/addRandom");
+    const { data } = await instance.post("/addRandom");
+    return data;
+  },
+  addCostum: async () => {
+    const { data } = await instance.post("/addCostum");
+    return data;
+  },
+  activatePromocode: async () => {
+    const { data } = await instance.post("/Activate");
+    return data;
+  },
+  usePromocode: async () => {
+    const { data } = await instance.post("/Use");
+    return data;
+  },
+  register: async () => {
+    const { data } = await instance.post("/Register");
+    return data;
+  },
+  login: async () => {
+    const { data } = await instance.post("/Login");
     return data;
   },
 };
