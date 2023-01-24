b085ff6a4742f9565429351f26467d8771b420d1
Fuad <fuadbayramv@gmail.com>
2023-01-24 14:29:35 +0400

    endpoints

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,40 +1,56 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "/api/Promocode",
+const Promocode = axios.create({
+  baseURL: "https://promocodepanelapi.inloya.com/api/Promocode",
 });
 
-const instanceLogin = axios.create({
-  baseURL: "/api/User",
+const Logins = axios.create({
+  baseURL: "https://promocodepanelapi.inloya.com/api/User",
+});
+
+const Merchant = axios.create({
+  baseURL: "https://promocodepanelapi.inloya.com/api/Merchant",
 });
 
 export const Apis = {
-  getAll: async () => {
-    const { data } = await instance.get("/All");
+  getAllMerchant: async () => {
+    const { data } = await Merchant.get("/All");
+    return data;
+  },
+  editMerchant: async () => {
+    const { data } = await Merchant.get("/Edit");
+    return data;
+  },
+  deleteMerchant: async () => {
+    const { data } = await Merchant.get("/Delete");
+    return data;
+  },
+  getAllPromocode: async () => {
+    const { data } = await Promocode.get("/All");
     return data;
   },
   addRandom: async () => {
-    const { data } = await instance.post("/addRandom");
+    const { data } = await Promocode.post("/addRandom");
     return data;
   },
   addCostum: async () => {
-    const { data } = await instance.post("/addCostum");
+    const { data } = await Promocode.post("/addCostum");
     return data;
   },
   activatePromocode: async () => {
-    const { data } = await instance.post("/Activate");
+    const { data } = await Promocode.post("/Activate");
     return data;
   },
   usePromocode: async () => {
-    const { data } = await instance.post("/Use");
+    const { data } = await Promocode.post("/Use");
     return data;
   },
   register: async () => {
-    const { data } = await instance.post("/Register");
+    const { data } = await Logins.post("/Register");
     return data;
   },
   login: async () => {
-    const { data } = await instance.post("/Login");
+    const { data } = await Logins.post("/Login");
     return data;
   },
 };
