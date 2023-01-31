d1e867f9ea1107508bbe7ddf67ee0a5f203c4a60
Fuad <fuadbayramv@gmail.com>
2023-02-01 02:25:15 +0400

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,9 @@ const Header = ({ title }) => {
 
   return (
     <div>
-      <div className={`${styles.Navbar} fixed z-20 w-9/12 bg-gray-100 px-2 `}>
+      <div
+        className={`${styles.Navbar} fixed z-20 xl:w-9/12 w-7/12 lg:w-8/12 2xl:w-10/12  bg-gray-100 px-2 `}
+      >
         <div className={styles.NavbarTitle}>
           <h3>{title}</h3>
         </div>
